c494c0f2016930a5a091860b3094b22835395710
Hydrogen <fanyangliang@gmail.com>
2018-09-21 12:40:39 +0800

    update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import HotTopic from './pages/HotTopic'
+// import HotTopic from './pages/HotTopic'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={HotTopic} />
+      {/* <Route path="/" component={HotTopic} /> */}
     </Switch>
   </BrowserRouter>
 )

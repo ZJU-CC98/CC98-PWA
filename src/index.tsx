d4d50b4dd3b4bf7392d63ceb7bfee1eb3b459053
Dearkano <vaynetian@cc98.org>
2018-11-03 16:04:40 +0800

    fix 暂时砍掉service work

--- src/index.tsx
+++ src/index.tsx
@@ -80,12 +80,12 @@ injectGlobal`
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration)
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError)
-    })
-  })
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//       console.log('SW registered: ', registration)
+//     }).catch(registrationError => {
+//       console.log('SW registration failed: ', registrationError)
+//     })
+//   })
+// }

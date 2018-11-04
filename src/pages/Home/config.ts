6e4521d050d740994ea96ab777d1239fc0120b51
adddna <zzaadddna@live.com>
2018-11-05 00:44:46 +0800

    fix: 删除一个主页背景
    哭

--- src/pages/Home/config.ts
+++ src/pages/Home/config.ts
@@ -1,20 +1,15 @@
 import asuka from '@/assets/asuka.jpg'
 import bg9 from '@/assets/bg9.jpg'
-import megumi from '@/assets/megumi.jpg'
 
 const Time = parseInt(Date.now().toString(), 10)
 export let Background = ''
 export let HomeText = ''
-switch (Time % 3) {
+switch (Time % 2) {
   case 0:
-    Background = megumi
-    HomeText = 'Megu の CC98'
-    break
-  case 1:
     Background = asuka
     HomeText = 'Asuka の CC98'
     break
-  case 2:
+  case 1:
     Background = bg9
     HomeText = '琪露诺 の CC98'
     break

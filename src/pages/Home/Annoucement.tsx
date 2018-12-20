4fe3d6286f84bb1fee9fa83c34ee32c92421edc2
vaynetian <vaynetian@cc98.org>
2018-12-20 15:10:04 +0800

    feat: 公告 暂时注释

--- src/pages/Home/Annoucement.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import UBB from '@/UBB'
-interface Props {
-  content: string
-}
-export default ({ content }: Props) => <div>{UBB(content)}</div>
---
+++ src/pages/Home/Announcement.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { ListItem, ListItemText, ListItemIcon, Divider } from '@material-ui/core'
+import AnnouncementIcon from '@material-ui/icons/Announcement'
+
+import UBB from '@/UBB'
+
+const DivS = styled.div`
+  margin: 1rem;
+`
+
+interface Props {
+  content: string
+}
+
+export default ({ content }: Props) => (
+  <>
+    <ListItem>
+      <ListItemIcon>
+        <AnnouncementIcon />
+      </ListItemIcon>
+      <ListItemText primary="推荐阅读" />
+    </ListItem>
+    <Divider />
+    <DivS>{UBB(content)}</DivS>
+  </>
+)
